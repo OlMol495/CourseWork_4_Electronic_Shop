0877906620f39331930f67431161665bc44bcfb8
Olga Lebedeva <olga.s.lebedeva@outlook.com>
2023-10-09 15:08:07 +0800

    fixed tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,7 +1,15 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
 from src.item import Item
+import pytest
 
 test_item = Item("Watches", 10000, 5) #test exemplar
+
+def test_str():
+    assert test_item.__str__() == "товар Watches, цена за единицу 10000, количество 5"
+
+
+def test_repr():
+    assert test_item.__repr__() == "Item('Watches', 10000, 5)"
 
 
 def test_calculate_total_price():
@@ -11,12 +19,8 @@ def test_calculate_total_price():
 
 def test_apply_discount():
     """checking price with discount"""
-    assert test_item.apply_discount() == 8000
+    test_item.apply_discount()
+    assert test_item.price == 8000
 
 
-def test_str():
-    assert test_item.__str__() == "товар Watches, цена за единицу 10000, количество 5"
 
-
-def test_repr():
-    assert test_item.__repr__() == "Item('Watches', 10000, 5)"
