9d199666d0b4825fae67b2db12c3c3b6ee76220d
Olga Lebedeva <olga.s.lebedeva@outlook.com>
2023-10-19 20:37:54 +0900

    fixed init for number_of_sim, added tests to test_phone

--- src/phone.py
+++ src/phone.py
@@ -3,22 +3,24 @@ from src.item import Item
 class Phone(Item):
     def __init__(self, name, price, amount, number_of_sim):
         super().__init__(name, price, amount)
-        self.__number_of_sim = number_of_sim
+        self.number_of_sim = number_of_sim
 
     def __repr__(self) -> str:
-        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.amount}, {self.__number_of_sim})"
+        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.amount}, {self.number_of_sim})"
 
     @property
     def number_of_sim(self):
         """getter for number_of_sim"""
-        return self.__number_of_sim
+        return self.number_of_sim
 
     @number_of_sim.setter
     def number_of_sim(self, number):
         """sets that number of sims should integer and bigger than zero"""
         if isinstance(number, int) and number > 0:
-            self.__number_of_sim = number
+            self.number_of_sim = number
         raise ValueError("Количество сим-карт должно быть целым числом и больше нуля")
+
+
 
 
 
